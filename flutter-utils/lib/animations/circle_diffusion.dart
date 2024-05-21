eb64fa685dd3741de84c71a6a5386e51814418c8
姚吉日 <gif@live.cn>
2024-05-21 11:37:15 +0800

    add two effects

---
+++ flutter-utils/lib/animations/circle_diffusion.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class CircleDiffusionEffect extends StatefulWidget {
+  static const String routeName = '/circleDiffusion';
+  final double startRadius;
+  final double endRadius;
+  final int color;
+  final double strokeWidth;
+  final int duration; //ms
+  final int interval; //ms
+
+  const CircleDiffusionEffect({
+    super.key,
+    required this.startRadius,
+    required this.endRadius,
+    required this.color,
+    required this.strokeWidth,
+    required this.duration,
+    required this.interval,
+  });
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const CircleDiffusionEffect(
+        startRadius: 50,
+        endRadius: 100,
+        color: 0xFF1FE7F5,
+        strokeWidth: 2,
+        duration: 700,
+        interval: 1500,
+      ),
+    );
+  }
+
+  @override
+  _CircleDiffusionEffectState createState() => _CircleDiffusionEffectState();
+}
+
+class _CircleDiffusionEffectState extends State<CircleDiffusionEffect> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late double lowerBound;
+  @override
+  void initState() {
+    super.initState();
+    lowerBound = widget.startRadius / widget.endRadius;
+    _controller = AnimationController(
+      lowerBound: lowerBound,
+      upperBound: 1,
+      vsync: this,
+      duration: Duration(milliseconds: widget.duration),
+    )..forward();
+
+    _controller.addListener(() async {
+      print(_controller.status);
+      if (_controller.isCompleted) {
+        await Future.delayed(Duration(seconds: 3));
+        _controller.forward(from: lowerBound);
+      } else if (_controller.isDismissed) {
+        // await Future.delayed(Duration(seconds: 3));
+        // _controller.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return CustomPaint(
+          painter:
+              _MyPainter(_controller.value * widget.endRadius, widget.color, widget.strokeWidth, _controller.value),
+          child: SizedBox(
+            width: widget.endRadius,
+            height: widget.endRadius,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void rePlay(BuildContext context) {
+    _controller.forward(from: lowerBound);
+  }
+}
+
+class _MyPainter extends CustomPainter {
+  final double radius;
+  final int color;
+  final double strokeWidth;
+  final double value;
+  _MyPainter(this.radius, this.color, this.strokeWidth, this.value);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Color(color).withAlpha((255 - (value * 255).round()).clamp(0, 255).toInt())
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(_MyPainter oldDelegate) {
+    return oldDelegate.value != value;
+  }
+}
---
+++ flutter-utils/lib/animations/rotating_circle.dart
@@ -0,0 +1,99 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class RotatingCircleEffect extends StatefulWidget {
+  static const String routeName = '/RotatingCircle';
+  final double radius;
+  final double strokeWidth;
+  final int duration;
+  final int color;
+  const RotatingCircleEffect(
+      {super.key, required this.radius, required this.duration, required this.strokeWidth, required this.color});
+
+  static Route route() {
+    //for test
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const RotatingCircleEffect(
+        radius: 50,
+        duration: 700,
+        strokeWidth: 3,
+        color: 0xFF0000FF,
+      ),
+    );
+  }
+
+  @override
+  _RotatingCircleEffectState createState() => _RotatingCircleEffectState();
+}
+
+class _RotatingCircleEffectState extends State<RotatingCircleEffect> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: widget.duration),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _CirclePainter(_controller, widget.radius, widget.strokeWidth, widget.color),
+      child: SizedBox(
+        // decoration: BoxDecoration(color: Palette.colorC),
+        width: widget.radius,
+        height: widget.radius,
+      ),
+    );
+  }
+}
+
+class _CirclePainter extends CustomPainter {
+  final Animation<double> animation;
+  final double radius;
+  final double strokeWidth;
+  final int color;
+  _CirclePainter(this.animation, this.radius, this.strokeWidth, this.color) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
+      ..isAntiAlias = true;
+
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final angle = 2 * pi * animation.value;
+    const tailLength = 1; // Tail length as a fraction of the full circle
+    const step = 0.01;
+    for (double i = 0; i < 1; i += step) {
+      final alpha = ((1 - i) * 255).toInt();
+      paint.color = Color(color).withAlpha(alpha);
+      final startAngle = angle - 2 * pi * i * tailLength;
+      final endAngle = startAngle + 2 * pi * step;
+
+      canvas.drawArc(
+        Rect.fromCircle(center: center, radius: radius),
+        startAngle,
+        endAngle - startAngle,
+        false,
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
