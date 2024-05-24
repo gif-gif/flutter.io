7a9a2870ed00d7ea03913a55aaa8eb6fbaf25e17
姚吉日 <gif@live.cn>
2024-05-24 11:11:09 +0800

    publish to pub.dev

--- flutter-lib/lib/flutter_lib.dart
+++
@@ -1,6 +0,0 @@
-library flutter_lib;
-
-export 'package:flutter_lib/animations/circle_diffusion.dart';
-export 'package:flutter_lib/animations/rotating_circle.dart';
-export 'package:flutter_lib/utils/crypto.dart';
-export 'package:flutter_lib/utils/time.dart';
---
+++ flutter-libs/lib/flutter_lib.dart
@@ -0,0 +1,6 @@
+library gif_flutter_libs;
+
+export 'package:gif_flutter_libs/animations/circle_diffusion.dart';
+export 'package:gif_flutter_libs/animations/rotating_circle.dart';
+export 'package:gif_flutter_libs/utils/crypto.dart';
+export 'package:gif_flutter_libs/utils/time.dart';
